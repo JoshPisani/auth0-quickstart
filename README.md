1efba61f813838978d2e9733f455619d2a71d1dc
JoshPisani <joshuapisani92@gmail.com>
2020-12-26 14:51:59 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# auth0-quickstart-project-k275fb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/auth0-quickstart-project-k275fb)
---
+++ app/auth/auth.config.ts
@@ -0,0 +1,15 @@
+interface AuthConfig {
+  CLIENT_ID: string;
+  CLIENT_DOMAIN: string;
+  AUDIENCE: string;
+  REDIRECT: string;
+  SCOPE: string;
+}
+
+export const AUTH_CONFIG: AuthConfig = {
+  CLIENT_ID: "15VoL1zQOUAVZdFhC4cOIUSfJDUYLdTZ",
+  CLIENT_DOMAIN: "shithead.au.auth0.com",
+  AUDIENCE: "https://shithead.au.auth0.com/userinfo",
+  REDIRECT: "https://auth0.stackblitz.io/callback",
+  SCOPE: "openid profile email"
+};
